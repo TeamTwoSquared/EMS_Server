f5a56d6ccdf3680cf5792c0ec73cedd389bc0815
sajun sandaruwan <sajunsandaruwan@gmail.com>
2019-01-08 05:20:25 +0530

    c7

--- public/notify.php
+++ public/notify.php
@@ -28,6 +28,7 @@ $merchant_secret = $row[0]; // Replace with your Merchant Secret (Can be found o
 $local_md5sig = strtoupper (md5 ( $merchant_id . $order_id . $payhere_amount . $payhere_currency . $status_code . strtoupper(md5($merchant_secret)) ) );
 if (($local_md5sig === $md5sig) AND ($status_code == 2) ){//success
 
+		date_default_timezone_set('Asia/Colombo');
 		$my_date = date("Y/m/d");;
 		$sql = "UPDATE ads SET isapprove = 1, last_payment_date = '$my_date' WHERE ad_id = '$order_id'";
 		$result = mysqli_query($connection,$sql);
