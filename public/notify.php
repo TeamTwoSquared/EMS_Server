a323d01a59349b8c32d5ce405dabb31d4cf5c451
sajun sandaruwan <sajunsandaruwan@gmail.com>
2019-01-08 04:14:24 +0530

    c4

--- public/notify.php
+++ public/notify.php
@@ -12,22 +12,29 @@ if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// $merchant_id         = $_POST['merchant_id'];
-// $order_id             = $_POST['order_id'];
-// $payhere_amount     = $_POST['payhere_amount'];
-// $payhere_currency    = $_POST['payhere_currency'];
-// $status_code         = $_POST['status_code'];
-// $md5sig                = $_POST['md5sig'];
+$merchant_id         = $_POST['merchant_id'];
+$order_id             = $_POST['order_id'];
+$payhere_amount     = $_POST['payhere_amount'];
+$payhere_currency    = $_POST['payhere_currency'];
+$status_code         = $_POST['status_code'];
+$md5sig                = $_POST['md5sig'];
 
 $query = "SELECT value FROM settings WHERE property = 'merchant_secret'";
 $result = mysqli_query($connection,$query);
 $row = mysqli_fetch_row($result);
-die($row[0]);
-$merchant_secret = 'XXXXXXXXXXXXX'; // Replace with your Merchant Secret (Can be found on your PayHere account's Settings page)
+
+$merchant_secret = $row[0]; // Replace with your Merchant Secret (Can be found on your PayHere account's Settings page)
 
 $local_md5sig = strtoupper (md5 ( $merchant_id . $order_id . $payhere_amount . $payhere_currency . $status_code . strtoupper(md5($merchant_secret)) ) );
 
-if (($local_md5sig === $md5sig) AND ($status_code == 2) ){
-        //TODO: Update your database as payment success
+if (($local_md5sig === $md5sig) AND ($status_code == 2) ){//success
+		$pay_date = date("Y/m/d");
+		
+		$sql = "UPDATE ads SET isapprove = 1, last_payment_date=$pay_date WHERE ad_id = $order_id";
+		$result = mysqli_query($connection,$sql);
+}
+else{
+	$sql = "UPDATE ads SET isapprove = 3 WHERE ad_id = $order_id";
+	$result = mysqli_query($connection,$sql);
 }
 ?>
